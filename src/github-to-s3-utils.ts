9d17a5b29b6d94bcc67f8d27b7d520dac825a662
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-26 21:46:25 -0600

    Get all assets from release (auto pagination)

--- src/github-to-s3-utils.ts
+++ src/github-to-s3-utils.ts
@@ -1,9 +1,11 @@
 import { S3Client } from '@aws-sdk/client-s3'
 import core from '@actions/core'
-import { Octokit } from '@octokit/rest'
+import { Octokit, type RestEndpointMethodTypes } from '@octokit/rest'
 import { Upload } from '@aws-sdk/lib-storage'
 import axios from 'axios'
 import type { SummaryTableRow } from '@actions/core/lib/summary.js'
+
+type GitHubReleaseAsset = RestEndpointMethodTypes['repos']['listReleaseAssets']['response']['data'][number]
 
 const uploadFileFromGitHubToS3 = async ({ url, bucket, objectKey, githubToken, s3Client }: { url: string; bucket: string; objectKey: string; githubToken: string; s3Client: S3Client }) => {
   try {
@@ -39,17 +41,37 @@ type UploadReleaseAssetsToS3Params = {
   }
 }
 
+export const listGithubReleaseAssets = async ({ githubToken, owner, repo, releaseId }: { githubToken: string; owner: string; repo: string; releaseId: number }): Promise<GitHubReleaseAsset[]> => {
+  try {
+    console.log('Will list all github release assets', { owner, repo, releaseId })
+    const octokit = new Octokit({ auth: githubToken })
+    let assets: GitHubReleaseAsset[] = []
+    let page = 1
+    let hasNextPage = true
+    while (hasNextPage) {
+      const response = await octokit.repos.listReleaseAssets({ owner, repo, release_id: releaseId, per_page: 100, page })
+      assets = assets.concat(response.data)
+      hasNextPage = response.headers?.link?.includes('rel="next"') || false
+      if (hasNextPage) {
+        page++
+      }
+    }
+    return assets
+  } catch (error) {
+    console.error('Unexpected error listing release assets', { owner, repo, releaseId, error })
+    throw new Error(`Unexpected error listing release assets: ${(error as Error).message}`, { cause: error })
+  }
+}
+
 export const uploadReleaseAssetsToS3 = async ({ githubToken, owner, repo, releaseId, s3: { endpoint, region, accessKeyId, secretAccessKey, bucket } }: UploadReleaseAssetsToS3Params): Promise<void> => {
   // List GitHub assets
-  const octokit = new Octokit({ auth: githubToken })
-  // TODO: Implement pagination since max 100 assets per req are returned.
-  const allGithubAssets = await octokit.repos.listReleaseAssets({ owner, repo, release_id: releaseId, per_page: 100 })
+  const allGithubAssets = await listGithubReleaseAssets({ githubToken, owner, repo, releaseId })
 
   // Transfer assets from GitHub to S3
-  console.log(`Will transfer ${allGithubAssets.data.length} from release "${releaseId}" to S3`)
+  console.log(`Will transfer ${allGithubAssets.length} from release "${releaseId}" to S3`)
   const s3Client = new S3Client({ endpoint, region, credentials: { accessKeyId, secretAccessKey } })
   const transfers = await Promise.all(
-    allGithubAssets.data.map(async (githubAsset): Promise<{ publicUrl: string; name: string }> => {
+    allGithubAssets.map(async (githubAsset): Promise<{ publicUrl: string; name: string }> => {
       console.log('Will upload', githubAsset.name)
       // The "githubAsset.browser_download_url" does not work, we need to build the download URL like so:
       const url = `https://api.github.com/repos/${owner}/${repo}/releases/assets/${githubAsset.id}`
@@ -59,7 +81,7 @@ export const uploadReleaseAssetsToS3 = async ({ githubToken, owner, repo, releas
       return { publicUrl, name: githubAsset.name }
     })
   )
-  console.log(`Did transfer ${allGithubAssets.data.length} from release "${releaseId}" to S3`)
+  console.log(`Did transfer ${allGithubAssets.length} from release "${releaseId}" to S3`)
 
   // Write summary
   core.summary.addHeading(`${transfers.length} release assets transferred from GitHub to S3`, 2)

00f59c91af49c7461d040e0ec04a55624676e44a
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-27 23:09:02 -0600

    Update generating summary

--- src/action-summary-utils.ts
+++ src/action-summary-utils.ts
@@ -1,28 +1,22 @@
 import * as core from '@actions/core'
 import { type SummaryTableRow } from '@actions/core/lib/summary'
 import { GitHubReleaseAsset } from './github-to-s3-utils'
+import { joinPaths, generateObjectUrlBase } from './s3-utils'
 
 const headers: SummaryTableRow = [{ data: 'Asset', header: true }]
 
 type WriteSummaryParams = {
-  assets: GitHubReleaseAsset[]
-  region: string
+  endpoint: string
   bucket: string
-  s3UrlTemplate: string
-  releaseId: number
+  folder: string
+  assets: GitHubReleaseAsset[]
 }
 
-export const writeSummary = async ({ assets, bucket, region, s3UrlTemplate, releaseId }: WriteSummaryParams): Promise<void> => {
+export const writeSummary = async ({ endpoint, bucket, folder, assets }: WriteSummaryParams): Promise<void> => {
+  const objectUrlBase = generateObjectUrlBase(endpoint, bucket)
   // Write summary
   core.summary.addHeading(`${assets.length} release assets transferred from GitHub to S3`, 2)
-  const tableData: SummaryTableRow[] = assets.map(asset => [`<a href="${getUrl({ region, bucket, template: s3UrlTemplate, filename: asset.name })}">${asset.name}</a>`])
+  const tableData: SummaryTableRow[] = assets.map(asset => [`<a href="${joinPaths(objectUrlBase, folder, asset.name)}">${asset.name}</a>`])
   core.summary.addTable([headers, ...tableData])
   await core.summary.write()
-  console.log('Did transfer from GitHub to S3', releaseId)
 }
-
-const getUrl = ({ bucket, region, filename, template }: { bucket: string; region: string; filename: string; template: string }): string =>
-  template
-    .replaceAll(/\{BUCKET}/gi, bucket)
-    .replaceAll(/\{REGION}/gi, region)
-    .replaceAll(/\{FILENAME}/gi, filename)
